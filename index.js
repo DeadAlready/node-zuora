7703ab02e5a8ab6a99b86082e1ee07852999a908
Karl Düüna <karl.dyyna@gmail.com>
2013-04-26 10:18:01 +0300

    Separate client, error handling

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 'use strict';
 
-var restify = require('restify');
-var assert = require('assert');
-var ZuoraClient = require('./lib/client');
+var Client = require('./lib/client');
+var Catalog = require('./lib/catalog');
+
+function Zuora(options) {
+    if(!(this instanceof Zuora)) {
+        return new Zuora(options);
+    }
+
+    var client = new Client(options);
+
+    this.catalog = new Catalog({client: client});
+}
 
 module.exports.create = function (opts) {
-
-    assert.ok((opts && typeof opts === 'object'), 'opts must be an object');
-//    assert.ok(opts.tenant, 'opts.tenant must be defined');
-    assert.ok(opts.user, 'opts.apiKey must be defined');
-    assert.ok(opts.password, 'opts.apiSecret must be defined');
-
-    var client = restify.createJsonClient({
-        url: opts.production ? 'https://api.zuora.com' : 'https://apisandbox-api.zuora.com',
-
-    });
-    client.basicAuth(opts.user, opts.password);
-
-    var options = {
-        client: client
-    };
-
-    return new ZuoraClient(options);
+    return new Zuora(opts);
 };
