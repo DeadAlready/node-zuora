2f86cfb20bea605a65e4726c48db0a1a2948cfed
Karl Düüna <karl.dyyna@gmail.com>
2013-09-16 14:42:04 +0300

    v0.0.9 - expose Subscription and fix some issues, add code comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Catalog = require('./lib/catalog');
 var Payment = require('./lib/payment');
 var Account = require('./lib/account');
 var Transaction = require('./lib/transaction');
+var Subscription = require('./lib/subscription');
 
 function Zuora(options) {
     if(!(this instanceof Zuora)) {
@@ -17,6 +18,7 @@ function Zuora(options) {
     this.account = new Account({client: client, validation: options.validation});
     this.payment = new Payment({client: client, validation: options.validation});
     this.transaction = new Transaction({client: client, validation: options.validation});
+    this.subscription = new Subscription({client: client, validation: options.validation});
 }
 
 module.exports.create = function (opts) {
